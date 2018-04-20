79742289deb0715f3c374ab65bd3f96f5bce0102
moyu <wy_865207357@163.com>
2018-04-21 00:22:42 +0800

    添加组件

---
+++ src/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <todo></todo>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./layout/header.vue"
+import Footer from "./layout/footer.jsx"
+import todo from './todo/todo.vue'
+
+export default {
+  components: {
+    Header,
+    Footer,
+    todo
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+#app {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+</style>
+
