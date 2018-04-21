6373f232a1da800f95b47da860206b38b7ca7b97
moyu <wy_865207357@163.com>
2018-04-21 13:59:55 +0800

    vender 分离

--- webpack.config.js
+++ webpack.config.js
@@ -106,6 +106,10 @@ if (isDev) {
     })
   )
 } else {
+  config.entry = {
+    app: path.join(__dirname, 'src/index.js'),
+    vendor: ['vue']
+  }
   config.output.filename = '[name].[chunkhash:8].js'
   config.module.rules.push(
     {
@@ -144,15 +148,34 @@ if (isDev) {
       ]
     }
   )
+
   config.optimization = {
     minimizer: [
       new UglifyJsPlugin({
         cache: true,
-        // paraller: true,
+        // paraller: true, // 未知问题，会报错
         sourceMap: true
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin()
+    ],
+    splitChunks: {
+      cacheGroups: {                  // 这里开始设置缓存的 chunks
+        commons: {
+          chunks: 'initial',      // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+          minSize: 0,             // 最小尺寸，默认0,
+          minChunks: 2,           // 最小 chunk ，默认1
+          maxInitialRequests: 5   // 最大初始化请求书，默认1
+        },
+        vendor: {
+          test: /node_modules/,   // 正则规则验证，如果符合就提取 chunk
+          chunks: 'initial',      // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+          name: 'vendor',         // 要缓存的 分隔出来的 chunk 名称
+          priority: 10,           // 缓存组优先级
+          enforce: true
+        }
+      }
+    },
+    runtimeChunk: true
   }
   config.plugins.push(
     new MiniCssExtractPlugin({
